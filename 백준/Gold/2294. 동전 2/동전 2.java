0bfa0ff0a94b0d244f07ef71cc57664d1f07c793
MindySo <mindyso7151@gmail.com>
2025-05-09 23:55:34 +0900

    [Gold V] Title: 동전 2, Time: 116 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final int INF = 10001;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] first = br.readLine().split(" ");
+        int n = Integer.parseInt(first[0]);
+        int k = Integer.parseInt(first[1]);
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        System.out.println(dp[k] == INF ? -1 : dp[k]);
+    }
+}
