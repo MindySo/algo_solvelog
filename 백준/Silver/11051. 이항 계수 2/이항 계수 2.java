5e0470b92e858f77f82e5c6d2f4a158ab4fd9707
So Minji <mindyso7151@gmail.com>
2025-06-30 22:30:21 +0900

    [Silver II] Title: 이항 계수 2, Time: 120 ms, Memory: 18000 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nk = br.readLine().split(" ");
+        int n = Integer.parseInt(nk[0]);
+        int k = Integer.parseInt(nk[1]);
+
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= Math.min(i, k); j++) {
+                if (j == 0 || j == i) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+                }
+            }
+        }
+
+        System.out.println(dp[n][k]);
+    }
+}
