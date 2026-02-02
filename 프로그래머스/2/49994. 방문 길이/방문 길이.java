9dc7baf7d3f44eb391aa15b6ab56c4d8db9c8e2c
So Minji <mindyso7151@gmail.com>
2026-02-02 13:10:27 +0900

    [level 2] Title: 방문 길이, Time: 0.14 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dirs) {
+        int[][] map = new int[11][11];
+        boolean[][] visited = new boolean[122][122];
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, 1, -1};
+        
+        int curr, r, c, nextR, nextC, d;
+        int next = 60;
+        int answer = 0;
+        
+        for(char dir : dirs.toCharArray()){
+            curr = next;
+            r = curr / 11;
+            c = curr % 11;
+            
+            if(dir == 'U'){
+                d = 0;
+            }else if(dir == 'D'){
+                d = 1;
+            }else if(dir == 'R'){
+                d = 2;
+            }else{
+                d = 3;
+            }
+            
+            nextR = r + dr[d];
+            nextC = c + dc[d];
+            
+            if(nextR >= 0 && nextR < 11 && nextC >= 0 && nextC < 11){
+                next = nextR * 11 + nextC;
+                if(!visited[curr][next]){
+                    visited[curr][next] = true;
+                    visited[next][curr] = true;
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
