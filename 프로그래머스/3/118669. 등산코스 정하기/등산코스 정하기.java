a67fedb577b87cd40c9dde480cfffaff5ed35e5d
Minji So <mindyso7151@gmail.com>
2026-03-27 14:06:34 +0900

    [level 3] Title: 등산코스 정하기, Time: 160.62 ms, Memory: 163 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -2,7 +2,6 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
         int[] dist = new int[n + 1];
-        Map<String, Integer> time = new HashMap();
         boolean[] visited = new boolean[n + 1];
         
         Set<Integer> summitSet = new HashSet();
@@ -11,13 +10,12 @@ class Solution {
         Set<Integer> gateSet = new HashSet();
         for(int g : gates) gateSet.add(g);
         
-        List<Integer>[] graph = new List[n + 1];
+        List<int[]>[] graph = new List[n + 1];
         for(int i = 0; i <= n; i++) graph[i] = new ArrayList();
         
         for(int[] p : paths){
-            graph[p[0]].add(p[1]);
-            graph[p[1]].add(p[0]);
-            time.put(p[0] + "/" + p[1], p[2]);
+            graph[p[0]].add(new int[] {p[1], p[2]});
+            graph[p[1]].add(new int[] {p[0], p[2]});
         }
         
         PriorityQueue<Integer> pq = new PriorityQueue(new Comparator<Integer>(){
@@ -29,38 +27,37 @@ class Solution {
         
         int totalMin = Integer.MAX_VALUE;
         int summitNum = Integer.MAX_VALUE;
-        
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
         for(int gate : gates){
-            pq.clear();
-            Arrays.fill(dist, Integer.MAX_VALUE);
-            Arrays.fill(visited, false);
-            
             dist[gate] = 0;
             pq.add(gate);
-            while(!pq.isEmpty()){
-                int curr = pq.poll();
-                
-                if(summitSet.contains(curr)){
-                    if(dist[curr] < totalMin){
-                        totalMin = dist[curr];
-                        summitNum = curr;
-                    }else if(dist[curr] == totalMin && curr < summitNum){
-                        summitNum = curr;
-                    }
-                    break;
+        }
+            
+        while(!pq.isEmpty()){
+            int curr = pq.poll();
+
+            if(summitSet.contains(curr)){
+                if(dist[curr] < totalMin){
+                    totalMin = dist[curr];
+                    summitNum = curr;
+                }else if(dist[curr] == totalMin && curr < summitNum){
+                    summitNum = curr;
                 }
-                
-                if(visited[curr]) continue;
-                visited[curr] = true;
-                
-                for(int next : graph[curr]){
-                    int t = time.get(Math.min(curr, next) + "/" + Math.max(curr, next));
-                    int newDist = Math.max(dist[curr], t);
-                    
-                    if(newDist < dist[next] && !visited[next]) {
-                        dist[next] = newDist;
-                        if(!gateSet.contains(next)) pq.add(next);
-                    }
+                continue;
+            }
+            
+            if(visited[curr]) continue;
+            visited[curr] = true;
+
+            for(int[] node : graph[curr]){
+                int next = node[0];
+                int t = node[1];
+                int newDist = Math.max(dist[curr], t);
+
+                if(newDist < dist[next]) {
+                    dist[next] = newDist;
+                    if(!visited[next]) pq.add(next);
                 }
             }
         }
