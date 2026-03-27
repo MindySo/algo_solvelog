cb347aa0c2a60bf30589bf177b74dcc893e3a6cc
Minji So <mindyso7151@gmail.com>
2026-03-27 11:32:25 +0900

    [level 3] Title: 스타 수열, Time: 337.03 ms, Memory: 178 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.java
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -1,40 +1,51 @@
 import java.util.*;
 class Solution {
     public int solution(int[] a) {
-        Map<Integer, Integer> map = new HashMap();
+        Map<Integer, List<Integer>> map = new HashMap();
         List<Integer> list = new ArrayList();
-        for(int i : a) {
-            if(!map.containsKey(i)) list.add(i);
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        for(int i = 0; i < a.length; i++) {
+            int num = a[i];
+            if(!map.containsKey(num)) {
+                list.add(num);
+                map.put(num, new ArrayList());
+            }
+            map.get(num).add(i);
         }
         
         Collections.sort(list, new Comparator<Integer>(){
             public int compare(Integer o1, Integer o2){
-                return map.get(o2) - map.get(o1);
+                return map.get(o2).size() - map.get(o1).size();
             }
         });
         
-        int answer = 0;
         
-        for(int i : list){
-            if(map.get(i) < answer) break;
+        boolean[] visited = new boolean[a.length];
+        int answer = 0;
+        for(int num : list){
+            Arrays.fill(visited, false);
+            if(map.get(num).size() <= answer) break;
             
-            int idx = 0;
+            List<Integer> idxs = map.get(num);
+            int i = 0;
             int cnt = 0;
             
-            while(idx + 1 < a.length){
-                if((a[idx] == i || a[idx + 1] == i) 
-                  && !(a[idx] == i && a[idx + 1] == i)){
+            while(i < idxs.size()){
+                int idx = idxs.get(i);
+                
+                if(idx > 0 && a[idx - 1] != num && !visited[idx - 1]) {
+                    visited[idx - 1] = true;
                     cnt++;
-                    idx += 2;
-                }else{
-                    idx++;
+                }else if(idx + 1 < a.length && a[idx + 1] != num && !visited[idx + 1]){
+                    visited[idx + 1] = true;
+                    cnt++;
                 }
+                
+                i++;
             }
             
-            answer = Math.max(answer, cnt * 2);
+            answer = Math.max(answer, cnt);
         }
 
-        return answer;
+        return answer * 2;
     }
 }
