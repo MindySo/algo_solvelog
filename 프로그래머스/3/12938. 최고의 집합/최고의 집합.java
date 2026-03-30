cd492cb04331478465d743673f82c34e15ad770a
Minji So <mindyso7151@gmail.com>
2026-03-30 19:11:23 +0900

    [level 3] Title: 최고의 집합, Time: 0.23 ms, Memory: 55.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n, int s) {
+        if(n == 1 || s < n) return new int[] {-1};
+        
+        int[] answer = new int[n];
+        for(int i = 0; i < n; i++){
+            answer[i] = s / (n - i);
+            s -= answer[i];
+        }
+        
+        return answer;
+    }
+}
