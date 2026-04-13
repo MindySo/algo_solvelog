61c89260b709a47b59f67509072aef8792c30096
Minji So <mindyso7151@gmail.com>
2026-04-13 13:03:41 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        int n = x;
+        int sum = 0;
+        while(n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        
+        if(x % sum == 0) return true;
+        return false;
+    }
+}
